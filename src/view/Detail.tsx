91ffc928e01363b8921f79e20c4d01fe87e1c9f9
tienkim9920 <tienkim9920@gmail.com>
2022-04-21 17:58:32 +0700

    update

--- src/view/Detail.tsx
+++ src/view/Detail.tsx
@@ -11,18 +11,13 @@ function Detail(props: any) {
         dispatch(decrement());
     }
 
-    const handlerTest = () => {
-        console.log('Test');
-    }
-
     return (
         <div className="mt-2 p-3">
             <div className='font-weight-bold color-main font-size-25 mt-2'>Nguyen Kim Tien</div>
             <div className='font-size-20 mt-2'>hello work</div>
             <div className='font-size-20 mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ut sapiente harum, commodi sequi numquam officia, 
             voluptas enim alias explicabo quos. Facilis ut aspernatur minus voluptatem perspiciatis illo earum nam.</div>
-
-            <button onClick={handlerTest}>TEST</button>
+            
             <button onClick={handlerDown}>Down</button>
             <div>{ count }</div>
             <button>Up</button>
