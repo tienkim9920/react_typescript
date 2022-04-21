6e0a6d4865fc9a8de88031ce0209e30966a75147
tienkim9920 <tienkim9920@gmail.com>
2022-04-21 11:41:44 +0700

    update

--- src/view/Detail.tsx
+++ src/view/Detail.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
+import { useAppSelector, useAppDispatch } from '../app/hooks';
+import { decrement, increment } from '../app/counter.redux';
 
 function Detail(props: any) {
+
+    const count = useAppSelector((state: any) => state.counter.value);
+    const dispatch = useAppDispatch();
+
+    const handleDown = () => {
+        dispatch(decrement());
+    }
+
     return (
         <div className="mt-2 p-3">
             <div className='font-weight-bold color-main font-size-25 mt-2'>Nguyen Kim Tien</div>
             <div className='font-size-20 mt-2'>hello work</div>
             <div className='font-size-20 mt-2'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti ut sapiente harum, commodi sequi numquam officia, 
             voluptas enim alias explicabo quos. Facilis ut aspernatur minus voluptatem perspiciatis illo earum nam.</div>
+
+            <button onClick={handleDown}>Down</button>
+            <div>{ count }</div>
+            <button>Up</button>
         </div>
     );
 }
